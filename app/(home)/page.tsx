20811f614233e967157e6c38f9e408adfe0c630a
Jashiro <loberanesjashen22@gmail.com>
2026-04-04 08:10:22 +0800

    added services section and fixed navbar

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/feature/home/HeroSection"
 import {TestimonialSection} from "@/components/feature/home/TestimonialGrid"
+import {ServicesSection} from "@/components/feature/home/ServiceSection"
 
 export default function Homepage(){
     return(
         <>
         <HeroSection/>
         <TestimonialSection/>
+        <ServicesSection/>
         </>
     )
 }
